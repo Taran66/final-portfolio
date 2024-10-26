f9eec56c5a9cb485209c589ca0d220e4198db3f5
Taran66 <taranpreet1911@gmail.com>
2024-10-26 07:33:06 +0530

    commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,39 +7,39 @@ export const navLinks = [
 
 export const Service = [
     {id: 1,
-    image: "../src/assets/frameworks.svg",
+    image: "/assets/frameworks.svg",
     heading: "Frontend Development",
     paragh: "Crafting pixel-perfect user interfaces with React, enhancing aesthetics through Tailwind CSS styling. Delivering responsive, modern web experiences for seamless cross-device functionality."},
 
     {id: 2,
-    image: "../src/assets/UiUx.svg",
+    image: "/assets/UiUx.svg",
     heading: "UI/UX Design",
     paragh: "Designing intuitive and visually captivating user interfaces, ensuring exceptional user experiences. Crafting seamless interactions through user-centric design principles and research."},
 
     {id: 3,
-    image: "../src/assets/server.svg",
+    image: "/assets/server.svg",
     heading: "Backend Development",
     paragh: "Expanding skillset to backend development, exploring Node.js and server-side technologies. Keen learner, actively acquiring knowledge to deliver comprehensive full-stack solutions."}
 ];
 
 export const Project = [
-    {image: "../src/assets/social-undraw.svg", 
+    {image: "/assets/social-undraw.svg", 
     projectName: "Social App", 
     paragh: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, alias.",
     video: "../src/assets/SocialApp.mp4",
     description: "Developed a comprehensive React-based social media platform featuring user-friendly functionalities such as adding friends, profile customization, post creation, and interactive features like liking and disliking posts. This project showcases my proficiency in React development and my ability to creat dynamic and engaging user experiences."},
 
-    {image: "../src/assets/nike_app.svg", 
+    {image: "/assets/nike_app.svg", 
     projectName: "Nike Clone Application",
     description: "A Nike Clone application where Customers can shop for Nike shoes. I used the JavaScript Front-End Framework React and Vite as a local development server which offers features like fast hot module replacement (HMR), optimized builds, and instant server startup. I used the Tailwind CSS to style the website.",
     paragh: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, alias.",
     video: "../src/assets/NikeApp.mp4"},
     
-    {image: "../src/assets/blog_app.svg", 
+    {image: "/assets/blog_app.svg", 
     projectName: "Blog Application (in production)", 
     description: "Daily Dive is a comprehensive Blogging application built using React as the Front-End framework , Vite as the local development server and Tailwind and Animate.css for the styling. In this application users can read the latest Blogs and perform basic CRUD functionalities on the application. Users have the flexibility to comment over the blogs and also share the blogs. It also includes Search Functionalities and also Markdown support. User Authentication and Authorization using Google account. Back-End will use technologies like NodeJs, ExpressJs and MongoDB.",
     paragh: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, alias.",
-    video: "../src/assets/DailyDive.mp4"}
+    video: "/assets/DailyDive.mp4"}
 ]
 
 
