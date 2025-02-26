842b8d9cb65c95fb0522955c0e25a931d61d0fe2
Taran66 <taranpreet1911@gmail.com>
2025-02-26 15:07:38 +0530

    final commit

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -23,23 +23,26 @@ export const Service = [
 ];
 
 export const Project = [
+    {image: "/assets/undraw_ai_chat.svg", 
+        projectName: "AI Assistant", 
+        description: "Made a comprehensive AI Assistant using LangChain (a software framework that helps facilitate the integration of large language models (LLMs) into applications). Used Groq chat model to make the AI model. Used Python for backend and React.js, TailwindCSS and Axios for frontend. User can also upload a document to get the answers related to the document.",
+        paragh: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, alias.",
+        video: "/assets/Askify.mp4"
+    },
+
     {image: "/assets/social-undraw.svg", 
     projectName: "Social App", 
     paragh: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, alias.",
     video: "assets/SocialApp.mp4",
-    description: "Developed a comprehensive React-based social media platform featuring user-friendly functionalities such as adding friends, profile customization, post creation, and interactive features like liking and disliking posts. This project showcases my proficiency in React development and my ability to creat dynamic and engaging user experiences."},
+    description: "Developed a comprehensive React-based social media platform featuring user-friendly functionalities such as adding friends, profile customization, post creation, and interactive features like liking and disliking posts. This project showcases my proficiency in React development and my ability to creat dynamic and engaging user experiences."
+},
 
     {image: "/assets/nike_app.svg", 
     projectName: "Nike Clone Application",
     description: "A Nike Clone application where Customers can shop for Nike shoes. I used the JavaScript Front-End Framework React and Vite as a local development server which offers features like fast hot module replacement (HMR), optimized builds, and instant server startup. I used the Tailwind CSS to style the website.",
     paragh: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, alias.",
-    video: "assets/NikeApp.mp4"},
-    
-    {image: "/assets/blog_app.svg", 
-    projectName: "Blog Application (in production)", 
-    description: "Daily Dive is a comprehensive Blogging application built using React as the Front-End framework , Vite as the local development server and Tailwind and Animate.css for the styling. In this application users can read the latest Blogs and perform basic CRUD functionalities on the application. Users have the flexibility to comment over the blogs and also share the blogs. It also includes Search Functionalities and also Markdown support. User Authentication and Authorization using Google account. Back-End will use technologies like NodeJs, ExpressJs and MongoDB.",
-    paragh: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae, alias.",
-    video: "/assets/DailyDive.mp4"}
+    video: "assets/NikeApp.mp4"
+},
 ]
 
 
